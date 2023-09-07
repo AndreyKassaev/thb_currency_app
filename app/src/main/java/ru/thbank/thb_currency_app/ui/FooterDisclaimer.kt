2dcb885ff4152d49b655b0ae38a876b9e61e91e2
Kassaev Andrew <git_public@mail.ru>
2023-09-07 17:05:30 +0400

    disclaimer commented

--- app/src/main/java/ru/thbank/thb_currency_app/ui/FooterDisclaimer.kt
+++ app/src/main/java/ru/thbank/thb_currency_app/ui/FooterDisclaimer.kt
@@ -4,16 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import ru.thbank.thb_currency_app.ui.theme.ForestGreen
-import ru.thbank.thb_currency_app.ui.theme.montserratFamily
 
 class FooterDisclaimer {
     @Composable
@@ -25,14 +19,14 @@ class FooterDisclaimer {
             horizontalArrangement = Arrangement.End,
             verticalAlignment = Alignment.CenterVertically
         ){
-            Text(
-                modifier = Modifier
-                    .padding(end = 16.dp),
-                text = "Не является публичной офертой",
-                color = Color.White,
-                fontWeight = FontWeight.Medium,
-                fontFamily = montserratFamily
-            )
+//            Text(
+//                modifier = Modifier
+//                    .padding(end = 16.dp),
+//                text = "Не является публичной офертой",
+//                color = Color.White,
+//                fontWeight = FontWeight.Medium,
+//                fontFamily = montserratFamily
+//            )
         }
     }
 
